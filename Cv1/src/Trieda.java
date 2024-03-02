0570de2ad0c0d9b86d93a2ca0da98fb37c879ec0
filip šupolík <filipsupolik25@gmail.com>
2024-03-02 11:04:54 +0100

    Class Trieda je hotova a funkcna

--- Cv1/src/Trieda.java
+++ Cv1/src/Trieda.java
@@ -26,10 +26,6 @@ public class Trieda {
         }
     }
 
-    public void setZoznamStudentov(Student[] zoznamStudentov) {
-        this.zoznamStudentov = zoznamStudentov;
-    }
-
     public boolean pridatStudenta(Student student) {
         boolean podariloSa = false;
         for (int i = 0; i < this.zoznamStudentov.length; i++) {
@@ -53,10 +49,41 @@ public class Trieda {
                 podariloSa = true;
                 break;
             }
-            if (!podariloSa) {
-                System.out.println("Nepodarilo sa pridat studenta podla mena");
+        }
+        if (!podariloSa) {
+            System.out.println("Nepodarilo sa pridat studenta podla mena");
+        }
+        return podariloSa;
+    }
+
+    public boolean odobratStudenta(String rodneCislo) {
+        boolean podariloSa = false;
+        for (int i = 0; i < this.zoznamStudentov.length; i++) {
+            if (this.zoznamStudentov[i].getRodneCislo().equals(rodneCislo)) {
+                this.zoznamStudentov[i] = null;
+                podariloSa = true;
             }
         }
         return podariloSa;
+    }
+
+    public void vypis() {
+        int poradie = 1;
+        System.out.printf("Trieda %s:%n", this.nazov);
+        for (int i = 0; i < this.zoznamStudentov.length; i++) {
+            if (this.zoznamStudentov[i] != null) {
+                System.out.printf("%d. %s %s%n", poradie, this.zoznamStudentov[i].getMeno(), this.zoznamStudentov[i].getPriezvisko());
+                poradie++;
+            } else {
+                int dalsiaPozicia = i + 1;
+                while (dalsiaPozicia < this.zoznamStudentov.length && this.zoznamStudentov[dalsiaPozicia] == null) {
+                    dalsiaPozicia++;
+                }
+                if (dalsiaPozicia < this.zoznamStudentov.length) {
+                    System.out.printf("%d. %s %s%n", poradie, this.zoznamStudentov[dalsiaPozicia].getMeno(), this.zoznamStudentov[dalsiaPozicia].getPriezvisko());
+                    poradie++;
+                }
+            }
+        }
     }
 }
